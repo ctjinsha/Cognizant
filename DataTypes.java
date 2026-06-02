31242f6e1dfe89962a54ea59a9b4ae193d337cc4
JINSHA MOL C T <ctjinshamol@gmail.com>
2026-06-02 23:36:23 +0530

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class DataTypes
+{
+    public static void main (String[] args)
+    {
+        
+        int number=10;
+        float avg=5.0f;
+        double mark=55.46;
+        char let='b';
+        boolean ans=true;
+        System.out.println("integer value:"+number);
+        System.out.println("flaoting  value:"+avg);
+        System.out.println("double value:"+mark);
+        System.out.println("character value:"+let);
+        System.out.println("boolean value:"+ans);
+       
+        
+    }
+}
