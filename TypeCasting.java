3fdd76970303423610fb769ce13d12b881625b00
JINSHA MOL C T <ctjinshamol@gmail.com>
2026-06-02 23:47:24 +0530

    Create TypeCasting.java

---
+++ TypeCasting.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class DataTypes
+{
+    public static void main (String[] args)
+    {
+        
+        int number=10;
+        double mark=1.5;
+        System.out.println("integer value"+(int)mark);
+        System.out.println("double value"+(double)number);
+       
+        
+    }
+}
