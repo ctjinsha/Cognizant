99554b158ac1399d7afe508a11f92c48f175e4f7
JINSHA MOL C T <ctjinshamol@gmail.com>
2026-06-28 23:53:08 +0530

    Create RecursiveFibonacci.java

---
+++ RecursiveFibonacci.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        System.out.println("fibonacci of"+n+"is"+fib(n));
+    }
+    public static int fib(int n)
+    {   
+        if (n==0 || n==1)
+        return n;
+        else
+        return fib(n-1)+fib(n);
+    }
+}
